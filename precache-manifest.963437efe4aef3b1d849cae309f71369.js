3432c7662f34e3803cbef790204f4f890aa88c83
Nikita <tudakov.nikita@gmail.com>
2021-01-26 12:07:05 +0200

    Updates

---
+++ precache-manifest.963437efe4aef3b1d849cae309f71369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6f92f6030de2ca4da705c73004d972",
+    "url": "./index.html"
+  },
+  {
+    "revision": "430a68838ec83aac909d",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "965d47e073d551d08910",
+    "url": "./static/js/2.98e7adc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.98e7adc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430a68838ec83aac909d",
+    "url": "./static/js/main.5746231f.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
